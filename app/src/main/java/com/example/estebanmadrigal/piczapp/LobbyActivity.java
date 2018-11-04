9a296c4ce925df3494aee55bd570a26f8be9bd87
madri308 <ememadrigal.em@gmail.com>
2018-11-04 07:22:31 -0600

    start

--- app/src/main/java/com/example/estebanmadrigal/piczapp/LobbyActivity.java
+++ app/src/main/java/com/example/estebanmadrigal/piczapp/LobbyActivity.java
@@ -1,7 +1,9 @@
 package com.example.estebanmadrigal.piczapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LobbyActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+    }
+    public void goToFeed(View view){
+        Intent intent = new Intent(this, FeedActivity.class);
+        startActivity(intent);
     }
 }
